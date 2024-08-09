44b247d3fa120dd3432a9679e88ea6f8714f9664
lucasoliveiraDEV22 <lucaszrones@gmail.com>
2024-08-09 20:58:54 -0300

    New repository

---
+++ scripts/script.js
@@ -0,0 +1,77 @@
+const textArea = document.querySelector('.input-text');
+const message = document.querySelector('.output-text');
+const copyButton = document.getElementById('btn-copiar');
+const noMessageElements = document.querySelector('.main_container_message').children;
+const outputSection = document.querySelector('.output');
+const mainContainerMessage = document.querySelector('.main_container_message');
+const img = document.querySelector('.img');
+const h2 = document.querySelector('.area_msg h2');
+const paragraph = document.querySelector('.text_message');
+
+
+const encryptionMatrix = [
+    ['e', 'enter'],
+    ['i', 'imes'],
+    ['a', 'ai'],
+    ['o', 'ober'],
+    ['u', 'ufat']
+];
+
+
+function encrypt(string) {
+    string = string.toLowerCase();
+    encryptionMatrix.forEach(pair => {
+        string = string.replaceAll(pair[0], pair[1]);
+    });
+    return string;
+}
+
+function decrypt(string) {
+    string = string.toLowerCase();
+    encryptionMatrix.forEach(pair => {
+        string = string.replaceAll(pair[1], pair[0]);
+    });
+    return string;
+}
+
+function btnEncrypt() {
+    const encryptedText = encrypt(textArea.value);
+    message.value = encryptedText;
+    textArea.value = '';
+    toggleVisibility(true);
+}
+
+function btnDecrypt() {
+    const decryptedText = decrypt(textArea.value);
+    message.value = decryptedText;
+    textArea.value = '';
+    toggleVisibility(true);
+}
+
+function toggleVisibility(isOutputVisible) {
+    if (isOutputVisible) {
+        outputSection.style.display = 'flex';
+        img.style.display = 'none';
+        h2.style.display = 'none';
+        paragraph.style.display = 'none';
+    } else {
+        outputSection.style.display = 'none';
+        img.style.display = 'block';
+        h2.style.display = 'block';
+        paragraph.style.display = 'block';
+    }
+}
+
+document.getElementById('btn-copiar').addEventListener('click', () => {
+    const textToCopy = message.value;
+    if (textToCopy) {
+        navigator.clipboard.writeText(textToCopy).then(() => {
+            message.value = '';
+            toggleVisibility(false);
+        }).catch(err => {
+            console.error('Erro ao copiar o texto: ', err);
+        });
+    }
+})
+// Inicializa a visibilidade correta ao carregar a página
+toggleVisibility(false);
