e7f2ab1e0dad05ede6f5ae5bad2ecd6660774169
lucasoliveiraDEV22 <lucaszrones@gmail.com>
2024-08-12 09:01:16 -0300

    New commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+// ELEMENTOS HTML REFERENCIADOS AO DOM
 const textArea = document.querySelector('.input-text');
 const message = document.querySelector('.output-text');
 const copyButton = document.getElementById('btn-copiar');
@@ -34,20 +35,21 @@ function decrypt(string) {
     return string;
 }
 
+// Função para criptografar a string
 function btnEncrypt() {
     const encryptedText = encrypt(textArea.value);
     message.value = encryptedText;
     textArea.value = '';
     toggleVisibility(true);
 }
-
+// Função para descriptografar a string
 function btnDecrypt() {
     const decryptedText = decrypt(textArea.value);
     message.value = decryptedText;
     textArea.value = '';
     toggleVisibility(true);
 }
-
+// Função para esconder a imagem e o texto fixo na direita
 function toggleVisibility(isOutputVisible) {
     if (isOutputVisible) {
         outputSection.style.display = 'flex';
@@ -62,6 +64,7 @@ function toggleVisibility(isOutputVisible) {
     }
 }
 
+// Função para dar funcionalidade ao botão "Copiar"
 document.getElementById('btn-copiar').addEventListener('click', () => {
     const textToCopy = message.value;
     if (textToCopy) {
